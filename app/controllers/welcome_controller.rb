4ee67bae7cd61f5aaa1a31f676c18c0e35cd6649
sandeep <acharya877@gmail.com>
2015-12-08 23:13:46 +0545

    csv processing and random quote working

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,24 @@
 class WelcomeController < ApplicationController
-  def index
-  end
 
-  def quote
-  	@quote = {:title =>'hello there', :author=>'sandeep'}
-  	respond_to do |format|
-  		format.html
-  		format.xml {render xml: @quote}
-  		format.json {render json: @quote}
-  	end
-  end
+	require 'csv'    
+	# array to store quote and its author as a dictionary {:title => 'title', :author => 'quote'}
+	@@quote_array = []
+	CSV.foreach('public/quotes.csv') do |row|
+		@@quote_array << [:title=>row[0], :author =>row[1]]
+	end
+
+	
+	def index
+	end
+
+	def quote
+		temp = rand(1..@@quote_array.count)		
+		# random entry from the array for view
+		@quote = @@quote_array[temp][0]
+		respond_to do |format|
+			format.html
+			format.xml {render xml: @quote}
+			format.json {render json: @quote}
+		end
+	end
 end
