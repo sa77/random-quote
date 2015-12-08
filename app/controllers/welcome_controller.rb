6be894e37d79e81d89b93cc865d830a903be49e2
sandeep <acharya877@gmail.com>
2015-12-08 21:25:53 +0545

    url routing for different data formats

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,13 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def quote
+  	@quote = {:title =>'hello there', :author=>'sandeep'}
+  	respond_to do |format|
+  		format.html
+  		format.xml {render xml: @quote}
+  		format.json {render json: @quote}
+  	end
+  end
 end
